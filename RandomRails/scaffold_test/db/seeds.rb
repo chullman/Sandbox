7a950d616e4aafe0af8e086f8844d192748d3b98
chullman <christopherhullman@gmail.com>
2018-03-23 11:24:34 +1000

    Practice rails mvc

---
+++ RandomRails/scaffold_test/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Desk Lamp", description: "A desk lamp", price: "50", brand: "Target")
+Product.create(name: "Shoes", description: "Black leather shoes", price: "100", brand: "Kmart")
