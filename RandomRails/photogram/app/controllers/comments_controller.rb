ec094ac31d80ab7e239b5fca23ee896eb160621b
chullman <christopherhullman@gmail.com>
2018-04-03 09:33:41 +1000

    Added comment validation

--- RandomRails/photogram/app/controllers/comments_controller.rb
+++ RandomRails/photogram/app/controllers/comments_controller.rb
@@ -8,14 +8,25 @@ end
 
 def create
   @comment = @post.comments.create(comment_params)
+
+  if @comment.content.empty?
+    flash[:alert] = "Comment can't be empty"
+    redirect_to posts_path
+    return
+  end
+
   @comment.user_id = current_user.id
+
 
   if @comment.save
     flash[:success] = "You commented the hell out of that post!"
     redirect_to posts_path
+  elsif @comment.content.empty?
+    flash[:alert] = "Comment can't be empty"
+    redirect_to posts_path
   else
     flash[:alert] = "Check the comment form, something went horribly wrong."
-    render root_path
+    redirect_to posts_path
   end
 end
 
--- RandomRails/photogram/app/controllers/posts_controller.rb
+++ RandomRails/photogram/app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @posts = @posts.order(created_at: :desc)
   end
 
   def new
