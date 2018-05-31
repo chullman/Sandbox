3593dfbffaf409ab1a8497e25d26d918611cc3c7
chullman <christopherhullman@gmail.com>
2018-05-31 10:35:37 +1000

    added base conv solution

---
+++ RandomJS/base_conv/base-conv.js
@@ -0,0 +1,37 @@
+let base10toAlphaHash = [
+  {1: "1"},
+  {2: "2"},
+  {3: "3"},
+  {4: "4"},
+  {5: "5"},
+  {6: "6"},
+  {7: "7"},
+  {8: "8"},
+  {9: "9"},
+  {10: "A"},
+  {11: "B"},
+  {12: "C"},
+  {13: "D"},
+  {14: "E"},
+  {15: "F"}
+];
+
+// The final Base 12 string
+let finalNum = "";
+
+function baseTenConv(baseTenNum, baseToInt) {
+  let quotient = Math.floor(baseTenNum / baseToInt);
+  let remainder = baseTenNum % baseToInt;
+    for (hash of base10toAlphaHash) {
+      if (remainder == Object.keys(hash)) {
+        finalNum = hash[Object.keys(hash)] + finalNum;
+        if (quotient != 0) {
+          baseTenConv(quotient, baseToInt);
+        }
+      }
+    }
+  return finalNum;
+}
+
+// Example - first argument is the base 10 number you want to convert, second argument is the base you want to convert to (e.g. base 12)
+console.log(baseTenConv(2535325, 12));
