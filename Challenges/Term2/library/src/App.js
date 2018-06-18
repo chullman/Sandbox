cf0db08370d7f6365dc4d8a93c38434b5c53b663
chullman <christopherhullman@gmail.com>
2018-06-18 15:30:41 +1000

    local storage done with library challenge

--- Challenges/Term2/library/src/App.js
+++ Challenges/Term2/library/src/App.js
@@ -36,17 +36,37 @@ class App extends Component {
       filter: event.target.value
     })
   }
-  handleSubmit(event) {
+  handleSearchSubmit(event) {
     event.preventDefault();
   }
+
+  handleAddSongSubmit = (event) => {
+    event.preventDefault();
+    let elements = event.target.elements;
+    let artist = elements.artist.value;
+    let title = elements.title.value;
+
+    this.setState({ 
+      songs: this.state.songs.concat([{"artist": artist, "title": title}])
+    }, this.addSongToLocalStorage(this.state.songs))
+
+  }
+
+  addSongToLocalStorage(songs) {
+    console.log("Now adding to local storage");
+    localStorage.setItem('songs', JSON.stringify(songs));
+  }
+
+
+
   render() {
     // Filter songs
-    const filterSongs = this.state.songs.filter(song => song.title.toLowerCase().includes(this.state.filter.toLowerCase()))
+    const filterSongs = JSON.parse(localStorage.getItem('songs')).filter(song => song.title.toLowerCase().includes(this.state.filter.toLowerCase()))
 
     return (
       <div className="App App-Custom-Background">
         <h1>Songify</h1>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSearchSubmit}>
           <label htmlFor="search">Search for song: </label>
           <input id="search" onChange={this.updateFilter} />
         </form>
@@ -54,6 +74,15 @@ class App extends Component {
         { filterSongs.map(song =>
           <Song artist={song.artist} title={song.title} />
         )}
+        <form onSubmit={this.handleAddSongSubmit}>
+          <h2>Add a song:</h2>
+          <label htmlFor="addArtist">Artist: </label>
+          <input id="addArtist" name="artist"/>
+          <label htmlFor="addTitle">Title: </label>
+          <input id="addTitle" name="title"/>
+          <button>Add</button>
+          <br/>
+        </form>
       </div>
     );
   }
